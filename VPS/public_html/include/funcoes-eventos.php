9382fea7fece37c6e3c6ea57846470f007d96e11
Paulo Addair <paulo@novaeratec.com.br>
2026-09-25 10:20:35 -0300

    fix(security): remove a chave de criptografia legada (encrypt_decrypt) do codigo
    A funcao encrypt_decrypt carregava a mesma chave da migracao HostGator escrita em texto claro
    neste arquivo, e nao tinha nenhum chamador -- nem no repositorio nem no site implantado. O bloco
    foi removido, a chave saiu do arquivo, e a sintaxe foi revalidada com php -l (codigo de saida 0).
    
    Mesma limpeza aplicada hoje em producao (de 17 arquivos para 1) e nos demais projetos. O unico
    consumidor vivo da chave e o DCOIN, que sera redesenhado com autenticacao pelo SSO.

--- VPS/public_html/include/funcoes-eventos.php
+++ VPS/public_html/include/funcoes-eventos.php
@@ -565,37 +565,6 @@ function chave($d = 3){
  * @param string $string The string value to encrypt or decrypt.
  * @return string
  */
-function encrypt_decrypt($action, $string)
-{
-  $output = false;
- 
-  $encrypt_method = "AES-256-CBC";
-  $secret_key = '!Fridahh#';
-  $secret_iv = 'novaeratec';
- 
-  // hash
-  $key = hash('sha256', $secret_key);
- 
-  // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a
-  // warning
-  $iv = substr(hash('sha256', $secret_iv), 0, 16);
- 
-  if ($action == 'encrypt')
-  {
-    $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-    $output = base64_encode($output);
-  }
-  else
-  {
-    if ($action == 'decrypt')
-    {
-      $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
-    }
-  }
- 
-  return $output;
-}
-
 	function strrevpos($instr, $needle)
 	{
 		$rev_pos = strpos (strrev($instr), strrev($needle));
